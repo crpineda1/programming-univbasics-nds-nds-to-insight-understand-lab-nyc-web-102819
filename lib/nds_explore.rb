ff1bda19936eda6753222719a12be0bdd8ec67b1
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-23 19:37:47 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,40 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
-
+#require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
+p directors_database[0][:name]
+
+p directors_database[0][:movies][0][:title]
+p directors_database[0][:movies][1][:title]
+p directors_database[0][:movies][2][:title]
+p directors_database[0][:movies][3][:title]
+p directors_database[0][:movies][4][:title]
+p directors_database[0][:movies][5][:title]
+
+p directors_database[0][:movies].count
+
 def print_first_directors_movie_titles
+  nds = directors_database
+  row = 0
+  speilbergMovies = []
+#pp direcotors_database[0][:movies][0][:title]
+
+  while row < nds.count do
+    if nds[row][:name] == "Stephen Spielberg"
+      element = 0
+      while element < nds[row][:movies].count do
+        speilbergMovies << nds[row][:movies][element][:title]
+          p nds[row][:movies][element][:title]
+        element += 1
+      end
+    else
+      row += 1
+    end
+  end
+  p speilbergMovies
 end
